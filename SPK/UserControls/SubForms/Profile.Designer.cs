a535b959b4f0f92fc9180492c829740a5a02eedd
Okpara Obinna <obainoforgod@gmail.com>
2018-11-14 16:09:06 +0100

    started new student control

--- SPK/UserControls/SubForms/Profile.Designer.cs
+++ SPK/UserControls/SubForms/Profile.Designer.cs
@@ -32,24 +32,24 @@
             this.lblTermNSession = new System.Windows.Forms.Label();
             this.lblSchool = new System.Windows.Forms.Label();
             this.panel3 = new System.Windows.Forms.Panel();
-            this.txtName = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
+            this.btnUploadStamp = new System.Windows.Forms.Button();
+            this.btnUploadLogo = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
             this.txtAddress = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.btnUploadLogo = new System.Windows.Forms.Button();
-            this.btnUploadStamp = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.picStamp = new System.Windows.Forms.PictureBox();
+            this.picLogo = new System.Windows.Forms.PictureBox();
             this.btnSave = new SPK.UserControls.Buttons.ButtonWithoutMenu();
             this.picSchoolLogo = new System.Windows.Forms.PictureBox();
-            this.picLogo = new System.Windows.Forms.PictureBox();
-            this.picStamp = new System.Windows.Forms.PictureBox();
             this.panel1.SuspendLayout();
             this.panel3.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.picSchoolLogo)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.picLogo)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.picStamp)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picLogo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picSchoolLogo)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
@@ -61,14 +61,14 @@
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1100, 168);
+            this.panel1.Size = new System.Drawing.Size(1100, 130);
             this.panel1.TabIndex = 1;
             // 
             // lblTermNSession
             // 
             this.lblTermNSession.AutoSize = true;
             this.lblTermNSession.Font = new System.Drawing.Font("Lucida Sans", 10F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblTermNSession.Location = new System.Drawing.Point(107, 87);
+            this.lblTermNSession.Location = new System.Drawing.Point(107, 75);
             this.lblTermNSession.Name = "lblTermNSession";
             this.lblTermNSession.Size = new System.Drawing.Size(184, 19);
             this.lblTermNSession.TabIndex = 2;
@@ -78,7 +78,7 @@
             // 
             this.lblSchool.AutoSize = true;
             this.lblSchool.Font = new System.Drawing.Font("Lucida Sans", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblSchool.Location = new System.Drawing.Point(149, 50);
+            this.lblSchool.Location = new System.Drawing.Point(149, 38);
             this.lblSchool.Name = "lblSchool";
             this.lblSchool.Size = new System.Drawing.Size(415, 31);
             this.lblSchool.TabIndex = 1;
@@ -101,41 +101,60 @@
             this.panel3.Controls.Add(this.txtName);
             this.panel3.Controls.Add(this.label1);
             this.panel3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.panel3.Location = new System.Drawing.Point(218, 198);
+            this.panel3.Location = new System.Drawing.Point(218, 189);
             this.panel3.Name = "panel3";
             this.panel3.Size = new System.Drawing.Size(650, 521);
             this.panel3.TabIndex = 3;
             // 
-            // txtName
+            // btnUploadStamp
             // 
-            this.txtName.Font = new System.Drawing.Font("Arial Rounded MT Bold", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtName.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(102)))), ((int)(((byte)(102)))), ((int)(((byte)(102)))));
-            this.txtName.Location = new System.Drawing.Point(205, 72);
-            this.txtName.Name = "txtName";
-            this.txtName.Size = new System.Drawing.Size(424, 27);
-            this.txtName.TabIndex = 1;
+            this.btnUploadStamp.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.btnUploadStamp.BackColor = System.Drawing.Color.White;
+            this.btnUploadStamp.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnUploadStamp.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUploadStamp.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(150)))), ((int)(((byte)(136)))));
+            this.btnUploadStamp.Location = new System.Drawing.Point(205, 346);
+            this.btnUploadStamp.Name = "btnUploadStamp";
+            this.btnUploadStamp.Size = new System.Drawing.Size(102, 41);
+            this.btnUploadStamp.TabIndex = 8;
+            this.btnUploadStamp.Text = "Browse...";
+            this.btnUploadStamp.UseVisualStyleBackColor = false;
             // 
-            // label1
+            // btnUploadLogo
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(102)))), ((int)(((byte)(102)))), ((int)(((byte)(102)))));
-            this.label1.Location = new System.Drawing.Point(41, 75);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(158, 20);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "SCHOOL NAME :";
+            this.btnUploadLogo.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.btnUploadLogo.BackColor = System.Drawing.Color.White;
+            this.btnUploadLogo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnUploadLogo.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUploadLogo.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(150)))), ((int)(((byte)(136)))));
+            this.btnUploadLogo.Location = new System.Drawing.Point(205, 211);
+            this.btnUploadLogo.Name = "btnUploadLogo";
+            this.btnUploadLogo.Size = new System.Drawing.Size(102, 41);
+            this.btnUploadLogo.TabIndex = 7;
+            this.btnUploadLogo.Text = "Browse...";
+            this.btnUploadLogo.UseVisualStyleBackColor = false;
             // 
-            // label2
+            // label5
             // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(42)))), ((int)(((byte)(42)))), ((int)(((byte)(42)))));
-            this.label2.Location = new System.Drawing.Point(230, 15);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(202, 23);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "SCHOOL DETAILS";
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Arial Rounded MT Bold", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(102)))), ((int)(((byte)(102)))), ((int)(((byte)(102)))));
+            this.label5.Location = new System.Drawing.Point(31, 356);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(168, 20);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "SCHOOL STAMP :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial Rounded MT Bold", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(102)))), ((int)(((byte)(102)))), ((int)(((byte)(102)))));
+            this.label4.Location = new System.Drawing.Point(42, 221);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(157, 20);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "SCHOOL LOGO :";
             // 
             // txtAddress
             // 
@@ -157,55 +176,52 @@
             this.label3.TabIndex = 3;
             this.label3.Text = "SCHOOL ADDRESS :";
             // 
-            // label4
+            // label2
             // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Arial Rounded MT Bold", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(102)))), ((int)(((byte)(102)))), ((int)(((byte)(102)))));
-            this.label4.Location = new System.Drawing.Point(42, 221);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(157, 20);
-            this.label4.TabIndex = 5;
-            this.label4.Text = "SCHOOL LOGO :";
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(42)))), ((int)(((byte)(42)))), ((int)(((byte)(42)))));
+            this.label2.Location = new System.Drawing.Point(230, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(202, 23);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "SCHOOL DETAILS";
             // 
-            // label5
+            // txtName
             // 
-            this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Arial Rounded MT Bold", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(102)))), ((int)(((byte)(102)))), ((int)(((byte)(102)))));
-            this.label5.Location = new System.Drawing.Point(31, 356);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(168, 20);
-            this.label5.TabIndex = 6;
-            this.label5.Text = "SCHOOL STAMP :";
+            this.txtName.Font = new System.Drawing.Font("Arial Rounded MT Bold", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtName.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(102)))), ((int)(((byte)(102)))), ((int)(((byte)(102)))));
+            this.txtName.Location = new System.Drawing.Point(205, 72);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(424, 27);
+            this.txtName.TabIndex = 1;
             // 
-            // btnUploadLogo
+            // label1
             // 
-            this.btnUploadLogo.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.btnUploadLogo.BackColor = System.Drawing.Color.White;
-            this.btnUploadLogo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnUploadLogo.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnUploadLogo.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(150)))), ((int)(((byte)(136)))));
-            this.btnUploadLogo.Location = new System.Drawing.Point(205, 211);
-            this.btnUploadLogo.Name = "btnUploadLogo";
-            this.btnUploadLogo.Size = new System.Drawing.Size(102, 41);
-            this.btnUploadLogo.TabIndex = 7;
-            this.btnUploadLogo.Text = "Browse...";
-            this.btnUploadLogo.UseVisualStyleBackColor = false;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(102)))), ((int)(((byte)(102)))), ((int)(((byte)(102)))));
+            this.label1.Location = new System.Drawing.Point(41, 75);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(158, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "SCHOOL NAME :";
             // 
-            // btnUploadStamp
+            // picStamp
             // 
-            this.btnUploadStamp.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.btnUploadStamp.BackColor = System.Drawing.Color.White;
-            this.btnUploadStamp.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnUploadStamp.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnUploadStamp.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(150)))), ((int)(((byte)(136)))));
-            this.btnUploadStamp.Location = new System.Drawing.Point(205, 346);
-            this.btnUploadStamp.Name = "btnUploadStamp";
-            this.btnUploadStamp.Size = new System.Drawing.Size(102, 41);
-            this.btnUploadStamp.TabIndex = 8;
-            this.btnUploadStamp.Text = "Browse...";
-            this.btnUploadStamp.UseVisualStyleBackColor = false;
+            this.picStamp.Location = new System.Drawing.Point(342, 308);
+            this.picStamp.Name = "picStamp";
+            this.picStamp.Size = new System.Drawing.Size(120, 120);
+            this.picStamp.TabIndex = 13;
+            this.picStamp.TabStop = false;
+            // 
+            // picLogo
+            // 
+            this.picLogo.Location = new System.Drawing.Point(342, 170);
+            this.picLogo.Name = "picLogo";
+            this.picLogo.Size = new System.Drawing.Size(120, 120);
+            this.picLogo.TabIndex = 12;
+            this.picLogo.TabStop = false;
             // 
             // btnSave
             // 
@@ -223,28 +239,12 @@
             // picSchoolLogo
             // 
             this.picSchoolLogo.Image = global::SPK.Properties.Resources.Icons8_flat_businessman_svg;
-            this.picSchoolLogo.Location = new System.Drawing.Point(103, 44);
+            this.picSchoolLogo.Location = new System.Drawing.Point(103, 32);
             this.picSchoolLogo.Name = "picSchoolLogo";
             this.picSchoolLogo.Size = new System.Drawing.Size(40, 40);
             this.picSchoolLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.picSchoolLogo.TabIndex = 0;
             this.picSchoolLogo.TabStop = false;
-            // 
-            // picLogo
-            // 
-            this.picLogo.Location = new System.Drawing.Point(342, 170);
-            this.picLogo.Name = "picLogo";
-            this.picLogo.Size = new System.Drawing.Size(120, 120);
-            this.picLogo.TabIndex = 12;
-            this.picLogo.TabStop = false;
-            // 
-            // picStamp
-            // 
-            this.picStamp.Location = new System.Drawing.Point(342, 308);
-            this.picStamp.Name = "picStamp";
-            this.picStamp.Size = new System.Drawing.Size(120, 120);
-            this.picStamp.TabIndex = 13;
-            this.picStamp.TabStop = false;
             // 
             // Profile
             // 
@@ -258,9 +258,9 @@
             this.panel1.PerformLayout();
             this.panel3.ResumeLayout(false);
             this.panel3.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.picSchoolLogo)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.picLogo)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.picStamp)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picLogo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picSchoolLogo)).EndInit();
             this.ResumeLayout(false);
 
         }
