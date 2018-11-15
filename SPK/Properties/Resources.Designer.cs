3d1ac1f812c977c5665f6e47130002abf795b846
Okpara Obinna <obainoforgod@gmail.com>
2018-11-15 10:00:06 +0100

    Register student subject

--- SPK/Properties/Resources.Designer.cs
+++ SPK/Properties/Resources.Designer.cs
@@ -323,6 +323,16 @@ namespace SPK.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap search_3 {
+            get {
+                object obj = ResourceManager.GetObject("search-3", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap staff {
             get {
                 object obj = ResourceManager.GetObject("staff", resourceCulture);
