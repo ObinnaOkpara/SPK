ba93449624b83c8cfb8932402d055be41b98e231
Okpara Obinna <obainoforgod@gmail.com>
2018-11-16 10:07:16 +0100

    View published result control

---
+++ SPK/UserControls/SubForms/ViewPublishedResult.Designer.cs
@@ -0,0 +1,298 @@
+﻿namespace SPK.UserControls.SubForms
+{
+    partial class ViewPublishedResult
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.dGridStudents = new System.Windows.Forms.DataGridView();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblSubTitle = new System.Windows.Forms.Label();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.picSchoolLogo = new System.Windows.Forms.PictureBox();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.cBoxSession = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.cBoxTerm = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.cBoxClass = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnSearch = new SPK.UserControls.Buttons.ButtonWithoutMenu();
+            this.btnDeleteAll = new SPK.UserControls.Buttons.ButtonWithoutMenu();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dGridStudents)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picSchoolLogo)).BeginInit();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel2
+            // 
+            this.panel2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.panel2.BackColor = System.Drawing.Color.White;
+            this.panel2.Controls.Add(this.btnDeleteAll);
+            this.panel2.Controls.Add(this.dGridStudents);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.panel2.Location = new System.Drawing.Point(73, 247);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(952, 527);
+            this.panel2.TabIndex = 21;
+            // 
+            // dGridStudents
+            // 
+            this.dGridStudents.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.dGridStudents.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dGridStudents.Location = new System.Drawing.Point(5, 35);
+            this.dGridStudents.Name = "dGridStudents";
+            this.dGridStudents.RowTemplate.Height = 24;
+            this.dGridStudents.Size = new System.Drawing.Size(942, 437);
+            this.dGridStudents.TabIndex = 15;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(42)))), ((int)(((byte)(42)))), ((int)(((byte)(42)))));
+            this.label3.Location = new System.Drawing.Point(384, 7);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(164, 23);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "SCORE SHEET";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.lblSubTitle);
+            this.panel1.Controls.Add(this.lblTitle);
+            this.panel1.Controls.Add(this.picSchoolLogo);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1100, 130);
+            this.panel1.TabIndex = 19;
+            // 
+            // lblSubTitle
+            // 
+            this.lblSubTitle.AutoSize = true;
+            this.lblSubTitle.Font = new System.Drawing.Font("Lucida Sans", 10F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSubTitle.Location = new System.Drawing.Point(107, 75);
+            this.lblSubTitle.Name = "lblSubTitle";
+            this.lblSubTitle.Size = new System.Drawing.Size(205, 19);
+            this.lblSubTitle.TabIndex = 2;
+            this.lblSubTitle.Text = "View Published Result";
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Lucida Sans", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.Location = new System.Drawing.Point(149, 38);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(339, 31);
+            this.lblTitle.TabIndex = 1;
+            this.lblTitle.Text = "RESULT MANAGEMENT";
+            // 
+            // picSchoolLogo
+            // 
+            this.picSchoolLogo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(150)))), ((int)(((byte)(136)))));
+            this.picSchoolLogo.Image = global::SPK.Properties.Resources.icons8_file_64;
+            this.picSchoolLogo.Location = new System.Drawing.Point(108, 31);
+            this.picSchoolLogo.Name = "picSchoolLogo";
+            this.picSchoolLogo.Size = new System.Drawing.Size(40, 40);
+            this.picSchoolLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picSchoolLogo.TabIndex = 0;
+            this.picSchoolLogo.TabStop = false;
+            // 
+            // panel3
+            // 
+            this.panel3.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.panel3.BackColor = System.Drawing.Color.White;
+            this.panel3.Controls.Add(this.btnSearch);
+            this.panel3.Controls.Add(this.cBoxSession);
+            this.panel3.Controls.Add(this.label6);
+            this.panel3.Controls.Add(this.cBoxTerm);
+            this.panel3.Controls.Add(this.label5);
+            this.panel3.Controls.Add(this.cBoxClass);
+            this.panel3.Controls.Add(this.label1);
+            this.panel3.Controls.Add(this.label2);
+            this.panel3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.panel3.Location = new System.Drawing.Point(74, 135);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(952, 106);
+            this.panel3.TabIndex = 23;
+            // 
+            // cBoxSession
+            // 
+            this.cBoxSession.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cBoxSession.FormattingEnabled = true;
+            this.cBoxSession.Items.AddRange(new object[] {
+            "--Select--"});
+            this.cBoxSession.Location = new System.Drawing.Point(517, 61);
+            this.cBoxSession.Name = "cBoxSession";
+            this.cBoxSession.Size = new System.Drawing.Size(214, 28);
+            this.cBoxSession.TabIndex = 87;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Arial Rounded MT Bold", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(102)))), ((int)(((byte)(102)))), ((int)(((byte)(102)))));
+            this.label6.Location = new System.Drawing.Point(513, 38);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(80, 20);
+            this.label6.TabIndex = 86;
+            this.label6.Text = "Session";
+            // 
+            // cBoxTerm
+            // 
+            this.cBoxTerm.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cBoxTerm.FormattingEnabled = true;
+            this.cBoxTerm.Items.AddRange(new object[] {
+            "--Select--",
+            "First Term",
+            "Second Term",
+            "Third Term"});
+            this.cBoxTerm.Location = new System.Drawing.Point(278, 61);
+            this.cBoxTerm.Name = "cBoxTerm";
+            this.cBoxTerm.Size = new System.Drawing.Size(212, 28);
+            this.cBoxTerm.TabIndex = 85;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Arial Rounded MT Bold", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(102)))), ((int)(((byte)(102)))), ((int)(((byte)(102)))));
+            this.label5.Location = new System.Drawing.Point(274, 38);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(56, 20);
+            this.label5.TabIndex = 84;
+            this.label5.Text = "Term";
+            // 
+            // cBoxClass
+            // 
+            this.cBoxClass.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cBoxClass.FormattingEnabled = true;
+            this.cBoxClass.Items.AddRange(new object[] {
+            "--Select--"});
+            this.cBoxClass.Location = new System.Drawing.Point(43, 61);
+            this.cBoxClass.Name = "cBoxClass";
+            this.cBoxClass.Size = new System.Drawing.Size(206, 28);
+            this.cBoxClass.TabIndex = 83;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(102)))), ((int)(((byte)(102)))), ((int)(((byte)(102)))));
+            this.label1.Location = new System.Drawing.Point(39, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 20);
+            this.label1.TabIndex = 81;
+            this.label1.Text = "Class";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(42)))), ((int)(((byte)(42)))), ((int)(((byte)(42)))));
+            this.label2.Location = new System.Drawing.Point(331, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(257, 23);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "View Published Results";
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(45)))), ((int)(((byte)(50)))));
+            this.btnSearch.BtnText = "SHOW";
+            this.btnSearch.ForeColor = System.Drawing.Color.White;
+            this.btnSearch.HoverColor = System.Drawing.Color.FromArgb(((int)(((byte)(8)))), ((int)(((byte)(15)))), ((int)(((byte)(23)))));
+            this.btnSearch.Icon = global::SPK.Properties.Resources.search_3;
+            this.btnSearch.Location = new System.Drawing.Point(769, 47);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.SideColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(125)))), ((int)(((byte)(113)))));
+            this.btnSearch.Size = new System.Drawing.Size(154, 49);
+            this.btnSearch.TabIndex = 88;
+            // 
+            // btnDeleteAll
+            // 
+            this.btnDeleteAll.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(45)))), ((int)(((byte)(50)))));
+            this.btnDeleteAll.BtnText = "Delete All";
+            this.btnDeleteAll.ForeColor = System.Drawing.Color.White;
+            this.btnDeleteAll.HoverColor = System.Drawing.Color.FromArgb(((int)(((byte)(8)))), ((int)(((byte)(15)))), ((int)(((byte)(23)))));
+            this.btnDeleteAll.Icon = global::SPK.Properties.Resources.icons8_cancel_64;
+            this.btnDeleteAll.Location = new System.Drawing.Point(380, 476);
+            this.btnDeleteAll.Name = "btnDeleteAll";
+            this.btnDeleteAll.SideColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(125)))), ((int)(((byte)(113)))));
+            this.btnDeleteAll.Size = new System.Drawing.Size(193, 46);
+            this.btnDeleteAll.TabIndex = 82;
+            // 
+            // ViewPublishedResult
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(229)))), ((int)(((byte)(229)))), ((int)(((byte)(229)))));
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "ViewPublishedResult";
+            this.Size = new System.Drawing.Size(1100, 780);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dGridStudents)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picSchoolLogo)).EndInit();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.DataGridView dGridStudents;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblSubTitle;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.PictureBox picSchoolLogo;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.ComboBox cBoxSession;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox cBoxTerm;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox cBoxClass;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private Buttons.ButtonWithoutMenu btnSearch;
+        private Buttons.ButtonWithoutMenu btnDeleteAll;
+    }
+}
