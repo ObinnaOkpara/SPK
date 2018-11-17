0b7083c8c976eaac08c81bbe5db8ed1660c85657
Okpara Obinna <obainoforgod@gmail.com>
2018-11-17 17:56:46 +0100

    Current Term and session

---
+++ SPK/UserControls/SubForms/CurrentTermNSession.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace SPK.UserControls.SubForms
+{
+    partial class CurrentTermNSession
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblTermNSession = new System.Windows.Forms.Label();
+            this.lblSchool = new System.Windows.Forms.Label();
+            this.picSchoolLogo = new System.Windows.Forms.PictureBox();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnSave = new SPK.UserControls.Buttons.ButtonWithoutMenu();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cBoxTerm = new System.Windows.Forms.ComboBox();
+            this.cBoxSession = new System.Windows.Forms.ComboBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picSchoolLogo)).BeginInit();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.lblTermNSession);
+            this.panel1.Controls.Add(this.lblSchool);
+            this.panel1.Controls.Add(this.picSchoolLogo);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 100);
+            this.panel1.TabIndex = 2;
+            // 
+            // lblTermNSession
+            // 
+            this.lblTermNSession.AutoSize = true;
+            this.lblTermNSession.Font = new System.Drawing.Font("Lucida Sans", 10F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTermNSession.Location = new System.Drawing.Point(23, 61);
+            this.lblTermNSession.Name = "lblTermNSession";
+            this.lblTermNSession.Size = new System.Drawing.Size(237, 16);
+            this.lblTermNSession.TabIndex = 2;
+            this.lblTermNSession.Text = "Save Current Term and Session";
+            // 
+            // lblSchool
+            // 
+            this.lblSchool.AutoSize = true;
+            this.lblSchool.Font = new System.Drawing.Font("Lucida Sans", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSchool.Location = new System.Drawing.Point(65, 24);
+            this.lblSchool.Name = "lblSchool";
+            this.lblSchool.Size = new System.Drawing.Size(330, 25);
+            this.lblSchool.TabIndex = 1;
+            this.lblSchool.Text = "ADMINISTRATIVE MANAGER";
+            // 
+            // picSchoolLogo
+            // 
+            this.picSchoolLogo.Image = global::SPK.Properties.Resources.Icons8_flat_businessman_svg;
+            this.picSchoolLogo.Location = new System.Drawing.Point(19, 18);
+            this.picSchoolLogo.Name = "picSchoolLogo";
+            this.picSchoolLogo.Size = new System.Drawing.Size(40, 40);
+            this.picSchoolLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picSchoolLogo.TabIndex = 0;
+            this.picSchoolLogo.TabStop = false;
+            // 
+            // panel3
+            // 
+            this.panel3.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.panel3.BackColor = System.Drawing.Color.White;
+            this.panel3.Controls.Add(this.cBoxSession);
+            this.panel3.Controls.Add(this.cBoxTerm);
+            this.panel3.Controls.Add(this.label3);
+            this.panel3.Controls.Add(this.btnSave);
+            this.panel3.Controls.Add(this.label2);
+            this.panel3.Controls.Add(this.label1);
+            this.panel3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.panel3.Location = new System.Drawing.Point(125, 142);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(550, 249);
+            this.panel3.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial Rounded MT Bold", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(102)))), ((int)(((byte)(102)))), ((int)(((byte)(102)))));
+            this.label3.Location = new System.Drawing.Point(79, 117);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(193, 16);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "Change Current Session";
+            // 
+            // btnSave
+            // 
+            this.btnSave.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(45)))), ((int)(((byte)(50)))));
+            this.btnSave.BtnText = "SAVE CHANGES";
+            this.btnSave.ForeColor = System.Drawing.Color.White;
+            this.btnSave.HoverColor = System.Drawing.Color.FromArgb(((int)(((byte)(8)))), ((int)(((byte)(15)))), ((int)(((byte)(23)))));
+            this.btnSave.Icon = global::SPK.Properties.Resources.icons8_save_64;
+            this.btnSave.Location = new System.Drawing.Point(152, 181);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.SideColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(125)))), ((int)(((byte)(113)))));
+            this.btnSave.Size = new System.Drawing.Size(250, 50);
+            this.btnSave.TabIndex = 15;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(42)))), ((int)(((byte)(42)))), ((int)(((byte)(42)))));
+            this.label2.Location = new System.Drawing.Point(124, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(289, 18);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Save Current Term and Session";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(102)))), ((int)(((byte)(102)))), ((int)(((byte)(102)))));
+            this.label1.Location = new System.Drawing.Point(79, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(172, 16);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Change Current Term";
+            // 
+            // cBoxTerm
+            // 
+            this.cBoxTerm.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cBoxTerm.FormattingEnabled = true;
+            this.cBoxTerm.Items.AddRange(new object[] {
+            "--Select--",
+            "First Term",
+            "Second Term",
+            "Third Term"});
+            this.cBoxTerm.Location = new System.Drawing.Point(83, 68);
+            this.cBoxTerm.Name = "cBoxTerm";
+            this.cBoxTerm.Size = new System.Drawing.Size(386, 24);
+            this.cBoxTerm.TabIndex = 80;
+            // 
+            // cBoxSession
+            // 
+            this.cBoxSession.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cBoxSession.FormattingEnabled = true;
+            this.cBoxSession.Items.AddRange(new object[] {
+            "--Select--"});
+            this.cBoxSession.Location = new System.Drawing.Point(82, 136);
+            this.cBoxSession.Name = "cBoxSession";
+            this.cBoxSession.Size = new System.Drawing.Size(386, 24);
+            this.cBoxSession.TabIndex = 81;
+            // 
+            // CurrentTermNSession
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Inherit;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(229)))), ((int)(((byte)(229)))), ((int)(((byte)(229)))));
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel1);
+            this.Name = "CurrentTermNSession";
+            this.Size = new System.Drawing.Size(800, 580);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picSchoolLogo)).EndInit();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblTermNSession;
+        private System.Windows.Forms.Label lblSchool;
+        private System.Windows.Forms.PictureBox picSchoolLogo;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label3;
+        private Buttons.ButtonWithoutMenu btnSave;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cBoxSession;
+        private System.Windows.Forms.ComboBox cBoxTerm;
+    }
+}
