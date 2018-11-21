77841234891c34d85ac57f5685c4399d4be9681a
Ofoedu Frank Ebuka <frankofoedu@gmail.com>
2018-11-21 13:11:43 +0100

    Finished backend of allocate fees

--- SPK/UserControls/SubForms/AllocateFees.cs
+++ SPK/UserControls/SubForms/AllocateFees.cs
@@ -18,6 +18,7 @@ namespace SPK.UserControls.SubForms
     {
         List<_class> classes = new List<_class>();
         List<fee_allocation> fees_allo = new List<fee_allocation>();
+        bool _cellformat = false;
 
         private IUnitOfWork _unitOfWork;
         public AllocateFees()
@@ -52,53 +53,60 @@ namespace SPK.UserControls.SubForms
             //  cBoxClass.Invalidate();
         }
 
-        private void dGridStudents_CellContentClick(object sender, DataGridViewCellEventArgs e)
-        {
-            var senderGrid = (DataGridView)sender;
+        //private void dGridStudents_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        //{
 
-            if (senderGrid.Columns[e.ColumnIndex] is DataGridViewButtonColumn && e.RowIndex >= 0)
-            {
-                var btn = (DataGridViewButtonColumn)senderGrid.Columns[e.ColumnIndex];
-                if (btn.Text == "Delete")
-                {
-                    var _id = (int)senderGrid.CurrentRow.Cells["id"].Value;
-                    using (var db = new Model1())
-                    {
-                        var feee_allo = new fee_allocation() { id = _id };
-                        db.fee_allocation.Attach(feee_allo);
-                        db.fee_allocation.Remove(feee_allo);
-                        db.SaveChanges();
+        //    var senderGrid = (DataGridView)sender;
+        //    var es = senderGrid.Rows[e.RowIndex].Cells[e.ColumnIndex];
 
-                        fees_allo = db.fee_allocation.ToList();
-                        dGridStudents.DataSource = fees_allo;
 
-                    }
-                }
-                else if(btn.Text == "Edit")
-                {
-                    var _id = (int)senderGrid.CurrentRow.Cells["id"].Value;
-                    using (var db = new Model1())
-                    {
-                       
-                        var feee_allo = new fee_allocation() { id = _id };
-                        db.fee_allocation.Attach(feee_allo);
-                        db.fee_allocation.Remove(feee_allo);
-                        db.SaveChanges();
+        //    if (senderGrid.Columns[e.ColumnIndex] is DataGridViewButtonColumn && e.RowIndex >= 0)
+        //    {
+        //        var btn = (DataGridViewButtonColumn)senderGrid.Columns[e.ColumnIndex];
 
-                        fees_allo = db.fee_allocation.ToList();
-                        dGridStudents.DataSource = fees_allo;
+        //        if (btn.Text == "Delete")
+        //        {
+        //            var _id = (int)senderGrid.CurrentRow.Cells["id"].Value;
+        //            using (var db = new Model1())
+        //            {
+        //                var feee_allo = new fee_allocation() { id = _id };
+        //                db.fee_allocation.Attach(feee_allo);
+        //                db.fee_allocation.Remove(feee_allo);
+        //                db.SaveChanges();
 
-                    }
-                }
-            }
-        }
+        //                fees_allo = db.fee_allocation.ToList();
+        //                dGridStudents.DataSource = fees_allo;
+
+        //            }
+        //        }
+        //        else if (btn.Text == "Edit")
+        //        {
+        //            btn.Text = "Update";
+        //        }
+        //    }
+
+        //}
 
         private void btnSave_ClickEvent(object sender, EventArgs e)
         {
             if (ValidateFomControls.CheckTextboxes(this, errorProvider1) &&
                 ValidateFomControls.CheckComboBoxes(this, errorProvider1))
             {
-
+                try
+                {
+                    var feea = CreateFeeAllocation();
+                    using (var db = new Model1())
+                    {
+                        db.fee_allocation.Add(feea);
+                        db.SaveChanges();
+                        dGridStudents.DataSource = db.fee_allocation.ToList();
+                        MessageBox.Show("New Fee Allocation Added");
+                    }
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("An error occured. Contact Support");
+                }
             }
 
         }
@@ -114,6 +122,63 @@ namespace SPK.UserControls.SubForms
             };
             return feeallo;
         }
+
+        private void dGridStudents_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (dGridStudents.Rows.Count > 0)
+            {
+                foreach (DataGridViewRow row in dGridStudents.SelectedRows) // foreach datagridview selected rows values  
+                {
+                    txtIDedit.Text = row.Cells[0].Value.ToString();
+                    txteditclas.Text = row.Cells[1].Value.ToString();
+                    txtedittype.Text = row.Cells[2].Value.ToString();
+                    txtFeeedit.Text = row.Cells[3].Value.ToString();
+                }
+            }
+        }
+
+        private void btnUpdate_ClickEvent(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace( txtFeeedit.Text))
+            {
+                using (var db = new Model1())
+                {
+                    var id = Convert.ToInt32(txtIDedit.Text);
+                    var feeallo = db.fee_allocation.Find(id);
+
+                    if (feeallo != null)
+                    {
+                        feeallo.fee_amount = Convert.ToInt32(txtFeeedit.Text);
+                        db.SaveChanges();
+                        MessageBox.Show("Fees Updated");
+                        dGridStudents.DataSource = db.fee_allocation.ToList();
+                    }
+                    else
+                    {
+                        MessageBox.Show("An error occured. Please contact support");
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please input a value in the field");
+            }
+           
+        }
+
+        //private void dGridStudents_CellFormatting(object sender, DataGridViewCellFormattingEventArgs e)
+        //    {
+        //        if (_cellformat == false)
+        //        {
+        //            foreach (DataGridViewRow Myrow in dGridStudents.Rows)
+        //            {
+        //                Myrow.Cells[5].Value = "Edit";
+
+        //            }
+        //        }
+        //      //  
+
+        //    }
     }
 }
 
