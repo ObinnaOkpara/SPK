2f4e5b259f33cf66567da261be9231b37df87517
Ofoedu Frank Ebuka <frankofoedu@gmail.com>
2018-11-23 17:40:28 +0100

    Finished secify number backend

--- SPK/UserControls/SubForms/SpecifySubjectNumber.cs
+++ SPK/UserControls/SubForms/SpecifySubjectNumber.cs
@@ -7,14 +7,123 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using SPK.Utilities;
+using DB;
 
 namespace SPK.UserControls.SubForms
 {
     public partial class SpecifySubjectNumber : UserControl
     {
+        int? _jssnumber;
+        int? _sssnumber;
         public SpecifySubjectNumber()
         {
             InitializeComponent();
+            backgroundWorker1.RunWorkerAsync();
+        }
+
+        private void btnSaveJss_ClickEvent(object sender, EventArgs e)
+        {
+            if (ValidateFomControls.CheckComboBoxes(this, errorProvider1))
+            {
+                using (var db = new Model1())
+                {
+                    var jssnum = db.jss_subject_number.FirstOrDefault();
+
+                    if (jssnum != null)
+                    {
+                        jssnum.number_of_subject = Convert.ToInt32(cBoxNum.Text);
+                        db.SaveChanges();
+                        lblJSSNum.Text = string.Format("JSS Students must offer {0} subjects", cBoxNum.Text);
+                        MessageBox.Show("Jss Subject Number updated");
+                        cBoxNum.SelectedIndex = -1;
+                    }
+                    else
+                    {
+                        var _jnum = new jss_subject_number()
+                        {
+                            declared_date = DateTime.Now.ToString("d"),
+                            declared_time = DateTime.Now,
+                            number_of_subject = Convert.ToInt32(cBoxNum.Text)
+
+                        };
+
+                        
+                        db.jss_subject_number.Add(_jnum);
+                        db.SaveChanges();
+                        lblJSSNum.Text = string.Format("JSS Students must offer {0} subjects", cBoxNum.Text);
+                        MessageBox.Show("Jss Subject Number added");
+                        cBoxNum.SelectedIndex = -1;
+                    }
+                }
+            }
+        }
+
+        private void btnSaveSss_ClickEvent(object sender, EventArgs e)
+        {
+            if (ValidateFomControls.CheckComboBoxes(this, errorProvider1))
+            {
+                using (var db = new Model1())
+                {
+                    var sssnum = db.sss_subject_number.FirstOrDefault();
+
+                    if (sssnum != null)
+                    {
+                        sssnum.number_of_subject = Convert.ToInt32(cBoxNum.Text);
+                        db.SaveChanges();
+                        lblSSSnum.Text = string.Format("SSS Students must offer {0} subjects", cBoxNum.Text);
+                        MessageBox.Show("SSS Subject Number updated");
+                        cBoxNum.SelectedIndex = -1;
+                    }
+                    else
+                    {
+                        var _ssnum = new sss_subject_number()
+                        {
+                            declared_date = DateTime.Now.ToString("d"),
+                            declared_time = DateTime.Now,
+                            number_of_subject = Convert.ToInt32(cBoxNum.Text)
+
+                        };
+
+                        db.sss_subject_number.Add(_ssnum);
+                        db.SaveChanges();
+                        lblSSSnum.Text = string.Format("SSS Students must offer {0} subjects", cBoxNum.Text);
+                        MessageBox.Show("SSS Subject Number added");
+                        cBoxNum.SelectedIndex = -1;
+                    }
+                }
+            }
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            using (var db = new Model1())
+            {
+                _jssnumber = db.jss_subject_number.FirstOrDefault().number_of_subject;
+                _sssnumber = db.sss_subject_number.FirstOrDefault().number_of_subject;
+               
+            }
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (_jssnumber == null)
+            {
+                lblJSSNum.Text = "JSS Students must offer 0 subjects";
+            }
+            else
+            {
+                lblJSSNum.Text = string.Format("JSS Students must offer {0} subjects", _jssnumber);
+            }
+
+            if (_sssnumber == null)
+            {
+                lblSSSnum.Text = "SSS Students must offer 0 subjects";
+            }
+            else
+            {
+                lblSSSnum.Text = string.Format("SSS Students must offer {0} subjects", _sssnumber);
+            }
         }
     }
 }
