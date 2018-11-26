e6ffda345aeecee9daa7f4926292a02ca561c8f2
Okpara Obinna <obainoforgod@gmail.com>
2018-11-26 20:54:50 +0100

    Promote or demote student usercontrol done.

--- SPK/UserControls/SubForms/PromoteDemoteStudent.Designer.cs
+++ SPK/UserControls/SubForms/PromoteDemoteStudent.Designer.cs
@@ -28,10 +28,240 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblSubTitle = new System.Windows.Forms.Label();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.picSchoolLogo = new System.Windows.Forms.PictureBox();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.btnSave = new SPK.UserControls.Buttons.ButtonWithoutMenu();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cboxToClass = new System.Windows.Forms.ComboBox();
+            this._txtRegNumber = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnFind = new System.Windows.Forms.Button();
+            this._txtCurClass = new System.Windows.Forms.TextBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picSchoolLogo)).BeginInit();
+            this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.lblSubTitle);
+            this.panel1.Controls.Add(this.lblTitle);
+            this.panel1.Controls.Add(this.picSchoolLogo);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 100);
+            this.panel1.TabIndex = 3;
+            // 
+            // lblSubTitle
+            // 
+            this.lblSubTitle.AutoSize = true;
+            this.lblSubTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSubTitle.Location = new System.Drawing.Point(35, 63);
+            this.lblSubTitle.Name = "lblSubTitle";
+            this.lblSubTitle.Size = new System.Drawing.Size(164, 17);
+            this.lblSubTitle.TabIndex = 2;
+            this.lblSubTitle.Text = "Change student class";
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.Location = new System.Drawing.Point(77, 26);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(295, 26);
+            this.lblTitle.TabIndex = 1;
+            this.lblTitle.Text = "STUDENT MANAGEMENT";
+            // 
+            // picSchoolLogo
+            // 
+            this.picSchoolLogo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(150)))), ((int)(((byte)(136)))));
+            this.picSchoolLogo.Image = global::SPK.Properties.Resources.icons8_people_64;
+            this.picSchoolLogo.Location = new System.Drawing.Point(36, 19);
+            this.picSchoolLogo.Name = "picSchoolLogo";
+            this.picSchoolLogo.Size = new System.Drawing.Size(40, 40);
+            this.picSchoolLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picSchoolLogo.TabIndex = 0;
+            this.picSchoolLogo.TabStop = false;
+            // 
+            // panel3
+            // 
+            this.panel3.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.panel3.BackColor = System.Drawing.Color.White;
+            this.panel3.Controls.Add(this._txtCurClass);
+            this.panel3.Controls.Add(this.btnFind);
+            this.panel3.Controls.Add(this.label3);
+            this.panel3.Controls.Add(this._txtRegNumber);
+            this.panel3.Controls.Add(this.cboxToClass);
+            this.panel3.Controls.Add(this.label1);
+            this.panel3.Controls.Add(this.btnSave);
+            this.panel3.Controls.Add(this.label4);
+            this.panel3.Controls.Add(this.label2);
+            this.panel3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.panel3.Location = new System.Drawing.Point(38, 139);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(724, 289);
+            this.panel3.TabIndex = 18;
+            // 
+            // btnSave
+            // 
+            this.btnSave.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(45)))), ((int)(((byte)(50)))));
+            this.btnSave.BtnText = "SAVE";
+            this.btnSave.ForeColor = System.Drawing.Color.White;
+            this.btnSave.HoverColor = System.Drawing.Color.FromArgb(((int)(((byte)(8)))), ((int)(((byte)(15)))), ((int)(((byte)(23)))));
+            this.btnSave.Icon = global::SPK.Properties.Resources.icons8_save_64;
+            this.btnSave.Location = new System.Drawing.Point(289, 211);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.SideColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(125)))), ((int)(((byte)(113)))));
+            this.btnSave.Size = new System.Drawing.Size(158, 44);
+            this.btnSave.TabIndex = 80;
+            this.btnSave.ClickEvent += new System.EventHandler(this.btnSave_ClickEvent);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial Rounded MT Bold", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(102)))), ((int)(((byte)(102)))), ((int)(((byte)(102)))));
+            this.label4.Location = new System.Drawing.Point(131, 117);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(124, 16);
+            this.label4.TabIndex = 78;
+            this.label4.Text = "Current Class :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(42)))), ((int)(((byte)(42)))), ((int)(((byte)(42)))));
+            this.label2.Location = new System.Drawing.Point(218, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(301, 18);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "PROMOTE OR DEMOTE STUDENT";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(102)))), ((int)(((byte)(102)))), ((int)(((byte)(102)))));
+            this.label1.Location = new System.Drawing.Point(111, 157);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(144, 17);
+            this.label1.TabIndex = 84;
+            this.label1.Text = "Destination Class :";
+            // 
+            // cboxToClass
+            // 
+            this.cboxToClass.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboxToClass.FormattingEnabled = true;
+            this.cboxToClass.Items.AddRange(new object[] {
+            "--Select--"});
+            this.cboxToClass.Location = new System.Drawing.Point(265, 154);
+            this.cboxToClass.Name = "cboxToClass";
+            this.cboxToClass.Size = new System.Drawing.Size(252, 24);
+            this.cboxToClass.TabIndex = 85;
+            // 
+            // _txtRegNumber
+            // 
+            this._txtRegNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this._txtRegNumber.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(102)))), ((int)(((byte)(102)))), ((int)(((byte)(102)))));
+            this._txtRegNumber.Location = new System.Drawing.Point(265, 66);
+            this._txtRegNumber.Name = "_txtRegNumber";
+            this._txtRegNumber.Size = new System.Drawing.Size(252, 23);
+            this._txtRegNumber.TabIndex = 91;
+            this._txtRegNumber.TextChanged += new System.EventHandler(this._txtRegNumber_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial Rounded MT Bold", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(102)))), ((int)(((byte)(102)))), ((int)(((byte)(102)))));
+            this.label3.Location = new System.Drawing.Point(142, 70);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(113, 16);
+            this.label3.TabIndex = 92;
+            this.label3.Text = "Reg Number :";
+            // 
+            // btnFind
+            // 
+            this.btnFind.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.btnFind.BackColor = System.Drawing.Color.White;
+            this.btnFind.Enabled = false;
+            this.btnFind.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnFind.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFind.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(150)))), ((int)(((byte)(136)))));
+            this.btnFind.Location = new System.Drawing.Point(537, 61);
+            this.btnFind.Name = "btnFind";
+            this.btnFind.Size = new System.Drawing.Size(102, 32);
+            this.btnFind.TabIndex = 93;
+            this.btnFind.Text = "Find...";
+            this.btnFind.UseVisualStyleBackColor = false;
+            this.btnFind.Click += new System.EventHandler(this.btnFind_Click);
+            // 
+            // _txtCurClass
+            // 
+            this._txtCurClass.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this._txtCurClass.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(102)))), ((int)(((byte)(102)))), ((int)(((byte)(102)))));
+            this._txtCurClass.Location = new System.Drawing.Point(265, 113);
+            this._txtCurClass.Name = "_txtCurClass";
+            this._txtCurClass.ReadOnly = true;
+            this._txtCurClass.Size = new System.Drawing.Size(252, 23);
+            this._txtCurClass.TabIndex = 94;
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // PromoteDemoteStudent
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Inherit;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(229)))), ((int)(((byte)(229)))), ((int)(((byte)(229)))));
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel1);
+            this.Name = "PromoteDemoteStudent";
+            this.Size = new System.Drawing.Size(800, 580);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picSchoolLogo)).EndInit();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblSubTitle;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.PictureBox picSchoolLogo;
+        private System.Windows.Forms.Panel panel3;
+        private Buttons.ButtonWithoutMenu btnSave;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cboxToClass;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox _txtRegNumber;
+        private System.Windows.Forms.TextBox _txtCurClass;
+        private System.Windows.Forms.Button btnFind;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
     }
 }
--- SPK/UserControls/SubForms/PromoteDemoteStudent.cs
+++ SPK/UserControls/SubForms/PromoteDemoteStudent.cs
@@ -7,14 +7,82 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using DB;
+using SPK.Utilities;
 
 namespace SPK.UserControls.SubForms
 {
     public partial class PromoteDemoteStudent : UserControl
     {
+        List<_class> Classes = new List<_class>();
+
         public PromoteDemoteStudent()
         {
             InitializeComponent();
+            cboxToClass.Cursor = Cursors.WaitCursor;
+            backgroundWorker1.RunWorkerAsync();
+        }
+
+        private void _txtRegNumber_TextChanged(object sender, EventArgs e)
+        {
+            btnFind.Enabled = true;
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            using (var db = new Model1())
+            {
+                Classes = db.classes.ToList();
+            }
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            cboxToClass.DataSource = Classes;
+            cboxToClass.DisplayMember = "classes";
+            cboxToClass.Cursor = Cursors.Arrow;
+        }
+
+        private void btnFind_Click(object sender, EventArgs e)
+        {
+            using (var db = new Model1())
+            {
+                var stu = db.students.FirstOrDefault(x=>x.reg_number == _txtRegNumber.Text);
+
+                if (stu == null) MessageBox.Show("No student was found with this reg number.");
+                else
+                {
+                    _txtCurClass.Text = stu._class;
+                    btnFind.Enabled = false;
+                }
+            }
+        }
+
+        private void btnSave_ClickEvent(object sender, EventArgs e)
+        {
+            if (ValidateFomControls.CheckTextboxes(this, errorProvider1) && ValidateFomControls.CheckComboBoxes(this, errorProvider1))
+            {
+                if (btnFind.Enabled)
+                {
+                    MessageBox.Show("Click Find to make sure the reg number exists first.");
+                    return;
+                }
+                var rtn = MessageBox.Show($"Are you sure you want to move this student from {_txtCurClass.Text} to {cboxToClass.Text}?", "", MessageBoxButtons.YesNo);
+                if (rtn== DialogResult.Yes)
+                {
+                    using (var db = new Model1())
+                    {
+                        var stu = db.students.FirstOrDefault(x => x.reg_number == _txtRegNumber.Text);
+
+                        if (stu == null) MessageBox.Show("No student was found with this reg number.");
+                        else
+                        {
+                            stu._class = cboxToClass.Text;
+                            db.SaveChanges();
+                        }
+                    }
+                }
+            }
         }
     }
 }
